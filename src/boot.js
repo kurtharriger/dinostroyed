6c6d64e63cd6bdcc41255ad15cbd85364a2f54d0
Kurt Harriger <kharriger@rallydev.com>
2016-07-06 13:29:39 -0600

    metadata for ios, updated art

--- src/boot.js
+++ src/boot.js
@@ -5,19 +5,18 @@ class Boot extends Phaser.State {
   }
 
   create() {
+
     this.game.input.maxPointers = 1;
-    //this.game.scale.pageAlignHorizontally = true;
-    // if (this.game.device.desktop) {
-    //   this.game.scale.pageAlignHorizontally = true;
-    // } else {
-      this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-      // this.game.scale.minWidth =  480;
-      // this.game.scale.minHeight = 260;
-      // this.game.scale.maxWidth = 640;
-      // this.game.scale.maxHeight = 480;
-      //this.game.scale.forceOrientation(true);
-      //this.game.scale.setScreenSize(true);
-    //}
+    this.game.scale.pageAlignHorizontally = true;
+    // if (!this.game.device.desktop) {
+    //   this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    //   this.game.scale.minWidth =  480;
+    //   this.game.scale.minHeight = 260;
+    //   this.game.scale.maxWidth = 1024;
+    //   this.game.scale.maxHeight = 768;
+    //   this.game.scale.forceOrientation(true);
+    // //  this.game.scale.setScreenSize(true);
+    // }
     this.game.state.start('preloader');
   }
 
--- src/game.js
+++ src/game.js
@@ -2,15 +2,15 @@ class Game extends Phaser.State {
 
 
   create() {
-    //this.input.onDown.add(this.onInputDown, this);
+    this.input.onDown.add(this.onInputDown, this);
     this.background = this.add.sprite(1024, 768, 'Background');
   }
 
   update() {}
-  
-  // onInputDown() {
-  //   this.game.state.start('menu');
-  // }
+
+  onInputDown() {
+    this.game.state.start('menu');
+  }
 
 }
 
